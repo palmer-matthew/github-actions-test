6211404f5a15e48ec7a873e1c8245923ba04ded9
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-11-14 21:52:06 -0500

    Match Project Structure

---
+++ hello/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def print_hello():
+    print('Hello World')
+
+if __name__ == '__main__':
+    print_hello()
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+ 
+setup(
+    name="hello",
+    author="Matthew Palmer",
+    description="Test Python Executable",
+    packages=find_packages()
+)
